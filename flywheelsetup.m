78cccd303a9f294b1d298ee6958f8d3808d4a20f
edina <edina@RyanPC>
2022-11-29 17:31:09 -0600

    adding setup

---
+++ flywheelsetup.m
@@ -0,0 +1,37 @@
+function [flywheel] = flywheelsetup()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  FUNCTION NAME: flywheelsetup
+%
+%  PURPOSE - setup the basic diamensions of the flywheel
+%
+%  INPUT - N/A 
+%
+%  OUTPUT - flywheel structure
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  AUTHOR: Ryan Goepfrich   
+%  DATE: 11/29/22
+%
+%  DESCRIPTION OF LOCAL VARIABLES
+%   
+%   flywheel.thickness = thickness of the fly wheel (given)
+%   flywheel.diameter = diameter of the fly wheel (not given get to choose)
+%   flywheel.width = width of the fly wheel (given)
+%  
+% FUNCTIONS CALLED
+%
+%  START OF EXECUTABLE CODE
+%
+
+flywheel.thickness = 0.07; % [m]
+flywheel.diameter = 0.8; % [m] (choose random number for right now)
+flywheel.width = 0.05; % [m]
+
+flywheel.density =  7660; % [Kg/m^3]
+flywheel.modulus = 207; % [GPa]
+flywheel.sut = 420; % [MPa] 
+flywheel.sy = 350; % [MPa] 
+
+
+end
+
---
+++ powerpistonsetup.m
@@ -0,0 +1,27 @@
+function [powerpiston] = powerpistonsetup()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  FUNCTION NAME: flywheelsetup
+%
+%  PURPOSE - setup the basic diamensions of the flywheel
+%
+%  INPUT - N/A 
+%
+%  OUTPUT - flywheel structure
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  AUTHOR: Ryan Goepfrich   
+%  DATE: 11/29/22
+%
+%  DESCRIPTION OF LOCAL VARIABLES
+%   
+%   flywheel.thickness = thickness of the fly wheel (given)
+%   flywheel.diameter = diameter of the fly wheel (not given get to choose)
+%   flywheel.width = width of the fly wheel (given)
+%  
+% FUNCTIONS CALLED
+%
+%  START OF EXECUTABLE CODE
+%
+
+end
+
