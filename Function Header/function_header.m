baebb0cc962a539cd0b471d5cc7d8135b1e33bb4
Luke Mackinnon <macki101@umn.edu>
2022-12-01 11:32:28 -0600

    Added function header with our names. Copy and paste when starting a function.

---
+++ Function Header/function_header.m
@@ -0,0 +1,38 @@
+function [  ]  = fncname( )
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  FUNCTION NAME:
+%
+%  PURPOSE:
+%
+%  INPUT:
+%
+%
+%
+%
+%
+%
+%  OUTPUT:
+%
+%
+%  AUTHOR: Luke MacKinnon, Ryan Goepfrich, Mitchel Medvec, Charlie Morain
+%  DATE: 12/1/22
+%
+%  DESCRIPTION OF LOCAL VARIABLES:
+%
+%
+%
+%
+%
+%
+%  FUNCTIONS CALLED:
+%
+%
+%
+%
+%
+%
+%  START OF EXECUTABLE CODE
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
