499949259867c1c398cfcfcf6a0d086b20bfe3c3
Mitchel Medvec <medve048@umn.edu>
2022-12-03 14:38:06 -0600

    Creating Function for Power Method 1

--- PowerCalc/PowerMethod1.m
+++ PowerCalc/PowerMethod1.m
@@ -1,7 +1,28 @@
-function [outputArg1,outputArg2] = PowerMethod1(inputArg1,inputArg2)
-%POWERMETHOD1 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function power1 = PowerMethod1(total)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  FUNCTION NAME:
+%
+%  PURPOSE:
+%
+%  INPUT:
+%
+%  OUTPUT:
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  AUTHOR: Luke MacKinnon, Ryan Goepfrich, Mitchel Medvec, Charlie Morain
+%  DATE: 12/1/22
+%
+%  DESCRIPTION OF LOCAL VARIABLES:
+%
+%  FUNCTIONS CALLED:
+%
+%
+%  START OF EXECUTABLE CODE
+
+omega = 2000;                   % Average rotational velocity [rpm]
+
+work = int(total.pressure,total.specificvolume,0,2*pi);
+
+power1 = work*omega/60;
 end
 
