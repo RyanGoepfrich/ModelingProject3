bd9ae819e158dc3d6620e15d5dd9bd2dc07e42f4
Luke Mackinnon <macki101@umn.edu>
2022-12-03 16:08:27 -0600

    Added KE calculations

--- KE Calcs/calcKE.m
+++ KE Calcs/calcKE.m
@@ -1,4 +1,4 @@
-function [KE]  = calcThetas(theta, total)
+function [KE]  = calcKE(theta, total)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  FUNCTION NAME:
 %
@@ -19,17 +19,6 @@ function [KE]  = calcThetas(theta, total)
 %
 %  START OF EXECUTABLE CODE
 
-T_0 = total.torque - total.torqueAvg;
-cross = 0;
-thetas = [0 0];
-
-for m = 2:length(theta)
-    if T_0(m - 1)*T_0(m) < 0
-       cross = cross + 1;
-       thetas(cross) = m;
-    end
-end
-
-KE = trapz(theta(thetas(1):thetas(2)), total.torque(thetas(1):thetas(2)));
+KE = trapz(thetatotal.torque);
 
 end
