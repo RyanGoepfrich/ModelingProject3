f20e842ed995a1948073722ee796a60332514d85
morai085 <119443154+morai085@users.noreply.github.com>
2022-12-06 16:04:08 -0600

    Added Temperature Variation

--- Varying Parameters/PlotVaryingCR.m
+++ Varying Parameters/PlotVaryingCR.m
@@ -1,7 +1,40 @@
-function [outputArg1,outputArg2] = PlotVaryingCR(inputArg1,inputArg2)
-%PLOTVARYINGCR Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function []  = PlotVaryingCR(total)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  FUNCTION NAME:
+%
+%  PURPOSE:
+%
+%  INPUT:
+%
+%  OUTPUT:
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  AUTHOR: Luke MacKinnon, Ryan Goepfrich, Mitchel Medvec, Charlie Morain
+%  DATE: 12/1/22
+%
+%  DESCRIPTION OF LOCAL VARIABLES:
+%
+%  FUNCTIONS CALLED:
+%
+%
+%  START OF EXECUTABLE CODE
+
+figure('name', 'Changing Compression Ratio Plots')
+subplot(2,1,1)
+plot(total.CR, total.varyingFlywheelDiamCR);
+xlabel('Compression Ratio');
+ylabel('Flywheel Diameter');
+title('Compression ratio vs Flywheel Diameter')
+
+subplot(2,1,2)
+plot(total.CR, total.varyingPower1CR);
+xlabel('Compression Ratio');
+ylabel('Power [W]')
+title('Compression Ratio vs Power (method 1)')
+
+%subplot(2,1,1)
+%plot(total.CR, total.varyingPower2);
+%xlabel('Compression Ratio');
+%ylabel('Power [W]');
 end
 
--- Varying Parameters/VaryingTemp.m
+++ Varying Parameters/VaryingTemp.m
@@ -1,29 +1,23 @@
+function [flywheelDiam, power1, power2]  = VaryingTemp(HighTemperature)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  FUNCTION NAME: Project3MainCode
+%  FUNCTION NAME:
 %
-%  PURPOSE - 
+%  PURPOSE:
 %
-%  INPUT - 
+%  INPUT:
 %
-%  OUTPUT - 
+%  OUTPUT:
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  AUTHOR: Ryan Goepfrich   
-%  DATE: 11/29/22
+%  AUTHOR: Luke MacKinnon, Ryan Goepfrich, Mitchel Medvec, Charlie Morain
+%  DATE: 12/1/22
 %
-%  DESCRIPTION OF LOCAL VARIABLES
-% 
-%  
-% FUNCTIONS CALLED
+%  DESCRIPTION OF LOCAL VARIABLES:
+%
+%  FUNCTIONS CALLED:
+%
 %
 %  START OF EXECUTABLE CODE
-%
-
-close all
-clear
-clc
-close all
-%%
 theta = deg2rad(0): deg2rad(1) :deg2rad(360); 
 theta2 = 0: 1: 360;
 C_R = 1.58;
@@ -33,6 +27,7 @@ powerpiston = powerpistonsetup(theta);
 displacer = displacersetup(powerpiston);
 regenerator = regeneratorsetup();
 flywheel = flywheelsetup();
+displacer.temp = HighTemperature;
 
 %% Position, Velc, Accel Calcs
 powerpiston = PosVelAccelAnalysis(powerpiston);
@@ -62,16 +57,8 @@ total.torqueAvg = calcTorqueAvg(total, theta);
 %% Power Calcs
 [total.power1,total.omegaAvg] = PowerMethod1(total);
 total.power2 = PowerMethod2(total);
-disp('Power1')
-disp(total.power1)
-disp('Power2')
-disp(total.power2)
 
-%% Graphing Functions
-close all
 
-specificvolumevsPressureGraph(total)
-torquePlots(theta2, total)
 
 %% Determine KE and I
 total.KE = calcKE(theta, total);
@@ -79,36 +66,8 @@ total.cf = 0.002;
 flywheel.I = total.KE/(total.cf*(total.omegaAvg^2));
 flywheel.diameter = calcD(flywheel);
 
-%% Rotational Velocity Analysis
+flywheelDiam = flywheel.diameter;
+power1 = total.power1;
+power2 = total.power2;
 
-ode = @(theta, omega2) torqueTheta(theta, total.torqueAvg, total.heightmax, total.mass) / (flywheel.I*omega2);
-[theta3, omega2] = ode45(ode, [0, theta(end)], total.omegaAvg);
-
-omega_check = (1/(2*pi))*trapz(theta3, omega2);
-cf_check = (max(omega2) - min(omega2))/total.omegaAvg;
-
-omegaPlots(theta3, omega2)
-
-%% Varying Parameters anaylsis
-
-total.lowCR = 1.1;
-total.highCR = 2;
-total.CR = linspace(total.lowCR, total.highCR, 10);
-
-
-for i=1:numel(total.CR)
-     [total.varyingFlywheelDiamCR(i), total.varyingPower1CR(i), total.varyingPower2CR(i)] = VaryingParameter(total.CR(i));
 end
-
-lowTHigh = 700;
-HighTHigh = 1100;
-total.varyingTemp = linspace(lowTHigh, HighTHigh, 200);
-
-for i=1:numel(total.varyingTemp)
-     [total.varyingFlywheelDiamTemp(i), total.varyingPower1Temp(i), total.varyingPower2Temp(i)] = VaryingTemp(total.varyingTemp(i));
-end
-
-PlotVaryingCR(total);
-
-
-
