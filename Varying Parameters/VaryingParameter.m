1860c6e47e9b6bc7ce84f27dc6b5dfcd6f616682
morai085 <119443154+morai085@users.noreply.github.com>
2022-12-06 11:52:30 -0600

    Updated Varying parmaters

--- Varying Parameters/VaryingParameter.m
+++ Varying Parameters/VaryingParameter.m
@@ -1,4 +1,4 @@
-function [  ]  = VaryingParameter(C_R, theta, theta2, flywheel, regenerator, powerpiston, displacer, total )
+function [flywheelDiam, power1, power2]  = VaryingParameter(C_R)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  FUNCTION NAME:
 %
@@ -19,29 +19,55 @@ function [  ]  = VaryingParameter(C_R, theta, theta2, flywheel, regenerator, pow
 %
 %  START OF EXECUTABLE CODE
 
-Varying.heightmax = hcalc(powerpiston,regenerator,C_R);
+theta = deg2rad(0): deg2rad(1) :deg2rad(360); 
+theta2 = 0: 1: 360;
+
+%% Set-Up functions
+powerpiston = powerpistonsetup(theta);
+displacer = displacersetup(powerpiston);
+regenerator = regeneratorsetup();
+flywheel = flywheelsetup();
+
+%% Position, Velc, Accel Calcs
+powerpiston = PosVelAccelAnalysis(powerpiston);
+displacer = PosVelAccelAnalysis(displacer);
+total.heightmax = hcalc(powerpiston,regenerator,C_R);
+
+%% Volume Calcs (Note: Regenerator calc is done in setup)
 powerpiston = VolumePowerPiston(powerpiston,displacer);
 displacer = VolumeDisplacer(displacer,total);
 
+%% Mass Calcs (Note: Regenerator calc is done in setup) 
 powerpiston = massCalcPiston(powerpiston);
 displacer = massCalcDisplacer(displacer);
-Varying.mass = totalMassCalc(powerpiston,displacer,regenerator);
+total.mass = totalMassCalc(powerpiston,displacer,regenerator);
 
 %% Total Volume Calc.
 [total.volume,total.specificvolume] = totalVolumeCalc(displacer,powerpiston,regenerator,total);
 
 %% Pressure Calc.
-varying.pressure = pressurecalc(total,powerpiston,displacer,regenerator);
+total.pressure = pressurecalc(total,powerpiston,displacer,regenerator);
 
 %% Torque Calcs
-varying.force = forcecalc(total,powerpiston);
-varying.torque = calcTorque(varying.force, powerpiston);
-total.torqueAvg = calcTorqueAvg(varying, theta);
+total.force = forcecalc(total,powerpiston);
+total.torque = calcTorque(total.force, powerpiston);
+total.torqueAvg = calcTorqueAvg(total, theta);
 
-varying.KE = calcKE(theta, varying);
+%% Power Calcs
+[total.power1,total.omegaAvg] = PowerMethod1(total);
+total.power2 = PowerMethod2(total);
+
+
+
+%% Determine KE and I
+total.KE = calcKE(theta, total);
 total.cf = 0.002;
-flywheel.I = varying.KE/(total.cf*(total.omegaAvg^2));
+flywheel.I = total.KE/(total.cf*(total.omegaAvg^2));
 flywheel.diameter = calcD(flywheel);
+
+flywheelDiam = flywheel.diameter;
+power1 = total.power1;
+power2 = total.power2;
 
 end
 
